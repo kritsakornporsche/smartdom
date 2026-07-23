cb3bf45faeff35ed4caa578a48ad98edf0cc9530
kritsakorn <kritsakorn801@gmail.com>
2026-07-23 12:30:26 +0700

    fix: start dual servers on 3000 and 5993

--- run_server.bat
+++ run_server.bat
@@ -1,3 +1,4 @@
 @echo off
 cd /d D:\kritsakorn\smartdom
-"C:\nvm4w\nodejs\node.exe" "node_modules\next\dist\bin\next" start -p 5993 -H 0.0.0.0
+start /b "smartdom-3000" "C:\nvm4w\nodejs\node.exe" "node_modules\next\dist\bin\next" start -p 3000 -H 0.0.0.0
+start /b "smartdom-5993" "C:\nvm4w\nodejs\node.exe" "node_modules\next\dist\bin\next" start -p 5993 -H 0.0.0.0

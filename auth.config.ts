0a25658d92fedc3968f8205ab480fe3f5538d5b8
kritsakorn <kritsakorn801@gmail.com>
2026-07-23 20:05:13 +0700

    fix: add trustHost: true to authConfig object

--- auth.config.ts
+++ auth.config.ts
@@ -1,10 +1,12 @@
 import type { NextAuthConfig } from "next-auth";
 
 export const authConfig = {
+  trustHost: true,
   secret: 'A3B4C5D6E7F8G9H0I1J2K3L4M5N6O7P8Q9R0S1T2U3V4W5X6Y7Z8',
   pages: {
     signIn: '/signin',
   },
+
 
   callbacks: {
     authorized() {

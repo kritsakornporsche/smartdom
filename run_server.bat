a3f07bcea9c79a201a63f027e2f67312d170b734
kritsakorn <kritsakorn801@gmail.com>
2026-07-23 12:49:06 +0700

    fix: set run_server.bat to port 3000 matching router LAN Host Port

--- run_server.bat
+++ run_server.bat
@@ -1,4 +1,3 @@
 @echo off
 cd /d D:\kritsakorn\smartdom
-start /b "smartdom-3000" "C:\nvm4w\nodejs\node.exe" "node_modules\next\dist\bin\next" start -p 3000 -H 0.0.0.0
-start /b "smartdom-5993" "C:\nvm4w\nodejs\node.exe" "node_modules\next\dist\bin\next" start -p 5993 -H 0.0.0.0
+"C:\nvm4w\nodejs\node.exe" "node_modules\next\dist\bin\next" start -p 3000 -H 0.0.0.0

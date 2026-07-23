ba5633f4100edacd3707bb21460da319290e5db0
kritsakorn <kritsakorn801@gmail.com>
2026-07-23 12:30:10 +0700

    feat: run dual ports 3000 and 5993 in ecosystem config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,27 @@
 module.exports = {
   apps: [
     {
-      name: 'smartdom',
+      name: 'smartdom-3000',
       script: 'node_modules/next/dist/bin/next',
-      args: 'start -p 5993',
+      args: 'start -p 3000 -H 0.0.0.0',
       interpreter: 'node',
       cwd: 'D:/kritsakorn/smartdom',
       instances: 1,
       autorestart: true,
-      watch: false,
-      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3000,
+        HOSTNAME: '0.0.0.0'
+      }
+    },
+    {
+      name: 'smartdom-5993',
+      script: 'node_modules/next/dist/bin/next',
+      args: 'start -p 5993 -H 0.0.0.0',
+      interpreter: 'node',
+      cwd: 'D:/kritsakorn/smartdom',
+      instances: 1,
+      autorestart: true,
       env: {
         NODE_ENV: 'production',
         PORT: 5993,

f7b9a2c9227ada85a0c5c12a5d02f702677cacab
kritsakorn <kritsakorn801@gmail.com>
2026-08-08 00:45:34 +0700

    Switch smartdom to port 3000 (DDNS 5993) and preserve other PM2 apps

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,19 +1,19 @@
 module.exports = {
   apps: [
     {
-      name: 'smartdom-3001',
+      name: 'smartdom-3000',
       script: 'node_modules/next/dist/bin/next',
       interpreter: 'node',
-      args: 'start -p 3001 -H 0.0.0.0',
+      args: 'start -p 3000 -H 0.0.0.0',
       exec_mode: 'fork',
       cwd: 'C:/kritsakorn/smartdom',
       autorestart: true,
       env: {
         NODE_ENV: 'production',
-        PORT: 3001,
+        PORT: 3000,
         HOSTNAME: '0.0.0.0',
-        AUTH_URL: 'http://kritsakorn.thddns.net:5996',
-        NEXTAUTH_URL: 'http://kritsakorn.thddns.net:5996',
+        AUTH_URL: 'http://kritsakorn.thddns.net:5993',
+        NEXTAUTH_URL: 'http://kritsakorn.thddns.net:5993',
         AUTH_TRUST_HOST: 'true'
       }
     }
--- run_server.bat
+++ run_server.bat
@@ -1,5 +1,5 @@
 @echo off
 cd /d C:\kritsakorn\smartdom
-npx next start -p 3001 -H 0.0.0.0
+npx next start -p 3000 -H 0.0.0.0
 
 

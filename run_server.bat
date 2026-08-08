232829ef76c619f0f0bd290cbe8d3a2fa639f1b0
kritsakorn <kritsakorn801@gmail.com>
2026-08-08 17:07:41 +0700

    Add explicit PATH to run_server.bat

--- run_server.bat
+++ run_server.bat
@@ -1,4 +1,5 @@
 @echo off
+set PATH=C:\kritsakorn\smartdom\node_modules\.bin;C:\Program Files\nodejs;C:\Users\buain\AppData\Roaming\npm;%PATH%
 cd /d C:\kritsakorn\smartdom
 npx next start -p 3000 -H 0.0.0.0
 

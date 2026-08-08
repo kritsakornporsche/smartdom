0327d5a5ebbfd80232b175d4f657201d0063c8bc
kritsakorn <kritsakorn801@gmail.com>
2026-08-08 17:09:28 +0700

    Use direct node executable in run_server.bat

--- run_server.bat
+++ run_server.bat
@@ -1,6 +1,6 @@
 @echo off
-set PATH=C:\kritsakorn\smartdom\node_modules\.bin;C:\Program Files\nodejs;C:\Users\buain\AppData\Roaming\npm;%PATH%
+set PATH=C:\Program Files\nodejs;C:\Users\buain\AppData\Roaming\npm;%PATH%
 cd /d C:\kritsakorn\smartdom
-npx next start -p 3000 -H 0.0.0.0
+"C:\Program Files\nodejs\node.exe" "node_modules\next\dist\bin\next" start -p 3000 -H 0.0.0.0
 
 
